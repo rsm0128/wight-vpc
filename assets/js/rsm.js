adc44b3b1595d0dde08700b9aa49890a944b7ffa
Web Expert 1102 <rsm0128@hotmail.com>
2019-07-10 10:21:08 +0800

    before responsive, dummy content and pattern change

--- assets/js/rsm.js
+++ assets/js/rsm.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function() {
 	if ( jQuery('#vpc-ajax-container').length ) {
 		// create a header section and move h1 to header, add logo
-		jQuery('#vpc-ajax-container').before('<div id="vpc-header"></div>');
+		jQuery('#vpc-ajax-container').before('<div id="vpc-header"><style>.entry-content{padding:50px;margin-left:-50px;margin-right:-50px;border: solid 1px #666;}</style></div>');
 		jQuery('.page .entry-header').appendTo('#vpc-header');
 		jQuery('#vpc-header').append('<img src="https://siteview.app/projects/wight/wp-content/themes/wight-child/logo.png" alt="logo" class="vpc-logo">');
 
@@ -17,16 +17,17 @@ jQuery(document).ready(function() {
 
 function rsmUpdateProductInfo() {
 	var chked_option = '.vpc-options input:checked + label.custom';
-	var base = jQuery('.BaseTab').find(chked_option).data('o-title');
-	var color = jQuery('.Color').find(chked_option).data('o-title');
-	var pattern = jQuery('.Pattern').find(chked_option).data('o-title');
-	var shade = jQuery('.Shade').find(chked_option).data('o-title');
+	var base = removeParenthesis(jQuery('.BaseTab').find(chked_option).data('o-title'));
+	var color = removeParenthesis(jQuery('.Color').find(chked_option).data('o-title'));
+	var pattern = removeParenthesis(jQuery('.Pattern').find(chked_option).data('o-title'));
+	var shade = removeParenthesis(jQuery('.Shade').find(chked_option).data('o-title'));
+	var spec = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 	var html = '<div class="vpc-info-block">' + 
 			'<div class="info-row"><div class="info-label">BASE</div><div class="info-data">' + base + '</div></div>' +
-			'<div class="info-row"><div class="info-label">COLOR</div><div class="info-data">' + color + '</div></div>' +
-			'<div class="info-row"><div class="info-label">PATTERN</div><div class="info-data">' + pattern + '</div></div>' +
+			'<div class="info-row"><div class="info-label">COVER</div><div class="info-data">' + color + ' ' + pattern + '</div></div>' +
 			'<div class="info-row"><div class="info-label">SHADE</div><div class="info-data">' + shade + '</div></div>' +
+			'<div class="info-row"><div class="info-label">SPECS</div><div class="info-data">' + spec + '</div></div>' +
 		'</div>';
 
 	jQuery('.vpc-info-block').remove();
@@ -40,4 +41,11 @@ function rsmInitCongurator() {
 	jQuery('#vpc-save-btn').text('SAVE').appendTo('.vpc-action-row');
 	jQuery('#vpc-add-to-cart').text('BUY').appendTo('.vpc-action-row');
 	rsmUpdateProductInfo();
+}
+
+function removeParenthesis(str) {
+	str = str.trim();
+	if (str.charAt(0) == '(') str = str.substr(1);
+	if (str.substr(-1) == ')') str = str.substr(0, str.length - 1);
+	return str;
 }
