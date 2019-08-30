3480bbea176b8aad5a8edeb20ce3011a5b766946
Web Expert 1102 <rsm0128@hotmail.com>
2019-08-30 22:24:48 +0800

    basetype and shadetype added

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,43 @@ document.addEventListener("mousewheel", function(e){
 	}
 }, { passive: false });
 
-jQuery( document ).ready(function() {
-	/*------------ZOOM------------*/
-	var ul = jQuery('#vpc-preview');
-    var ulHeight = jQuery('#vpc-preview').outerHeight();
-
-	jQuery('body').on('mousemove', '#vpc-preview', function(e){
-		var win = jQuery('#vpc-preview');
-		var cST = win.scrollTop();
-
-		ulHeight = jQuery('#vpc-preview').outerHeight();
-
-		if ( e.pageY >= (ulHeight/1.2) ){
-			win.scrollTop(cST + 10);
-		} else {
-			win.scrollTop(cST - 10);
-		}
-	});
-	/*------------ZOOM------------*/
-
-	/*-----UP AND DOWN MOVE FOR PATTERN IMAGES----*/
-
+jQuery(document).ready(function() {
 
 	var state = 'no';
 	var scroll_state = 'yes';
 
+	// middle column
+	jQuery('body').on('click', '.vpc-component.BaseType .vpc-options label', function(){
+		jQuery('.vpc-component.Shade').hide();
+		jQuery('.vpc-component.Pattern').hide();
+		jQuery('.vpc-component.BaseTab').show();
+
+		jQuery('.vpc-tab-header.ShadeType').removeClass('label-active');
+		jQuery('.vpc-tab-header.Color').removeClass('label-active');
+		jQuery('.vpc-tab-header.BaseType').addClass('label-active');
+	});
+
+	jQuery('body').on('click', '.vpc-component.Color .vpc-options label', function(){
+		jQuery('.vpc-component.Shade').hide();
+		jQuery('.vpc-component.BaseTab').hide();
+		jQuery('.vpc-component.Pattern').show();
+
+		jQuery('.vpc-tab-header.ShadeType').removeClass('label-active');
+		jQuery('.vpc-tab-header.BaseType').removeClass('label-active');
+		jQuery('.vpc-tab-header.Color').addClass('label-active');
+	});
+
+	jQuery('body').on('click', '.vpc-component.ShadeType .vpc-options label', function(){
+		jQuery('.vpc-component.Shade').show();
+		jQuery('.vpc-component.Pattern').hide();
+		jQuery('.vpc-component.BaseTab').hide();
+
+		jQuery('.vpc-tab-header.Color').removeClass('label-active');
+		jQuery('.vpc-tab-header.BaseType').removeClass('label-active');
+		jQuery('.vpc-tab-header.ShadeType').addClass('label-active');
+	});
+
+	// mouse wheel activity
 	jQuery('body').on('mousemove','.Pattern .vpc-group', function(){
 		if(state == 'no'){
 			state = 'yes';
@@ -63,7 +75,8 @@ jQuery( document ).ready(function() {
 		}
 	});
 
-	jQuery( "body" ).on( "click", '.Pattern .vpc-group .vpc-single-option-wrap:not(:last-child) label', function(){
+	// scroll action to pattern
+	jQuery( "body" ).on( "click", '.Pattern .vpc-group .vpc-single-option-wrap label', function(){
 		//var curr_scroll = jQuery(this).parent().position().top;
 		var el_index = jQuery(this).closest('.vpc-group').find('label').index(jQuery(this));
 		if(el_index !== 1){
@@ -73,27 +86,57 @@ jQuery( document ).ready(function() {
 			jQuery(this).parents('.vpc-group').animate({ scrollTop: curr_scroll }, 600 );
 		}
 	});
-	/*-----UP AND DOWN MOVE FOR PATTERN IMAGES----*/
 
-	/*-----SHOW HIDE FOR TABS----*/
-	jQuery( "body" ).on( "click", '.vpc-tab-header', function(){
-		//alert('click to show options');
-		jQuery('.Color .vpc-component-header').hide();
-		jQuery('.Color .vpc-options').hide();
-		jQuery('.Shade .vpc-component-header').hide();
-		jQuery('.Shade .vpc-options').hide();
-		jQuery('.BaseTab .vpc-component-header').hide();
-		jQuery('.BaseTab .vpc-options').hide();
-		jQuery('.vpc-tab-header').css("background-color", "transparent");
+	if (jQuery('#vpc-ajax-container').length) {
+		// create a header section and move h1 to header, add logo
+		jQuery('#vpc-ajax-container').before('<div id="vpc-header"><style>.entry-content{padding:50px;margin-left:-50px;margin-right:-50px;border: solid 1px #666;}</style></div>');
+		jQuery('.page .entry-header').appendTo('#vpc-header');
+		jQuery('#vpc-header').append('<img src="https://siteview.app/projects/wight/wp-content/themes/wight-child/logo.png" alt="logo" class="vpc-logo">');
 
-		jQuery( this ).siblings('.vpc-options').show();
-		jQuery( this ).css("background-color", "#f1f1f1");
+		// update product info
+		jQuery(document).on('click', 'label.custom', function () {
+			rsmUpdateProductInfo();
+		});
 
-		if ( jQuery( this ).hasClass( "Color" ) ) {
-			jQuery( '.vpc-component.Pattern' ).show();
-		}else{
-			jQuery( '.vpc-component.Pattern' ).hide();
-		}
-	});
-	/*-----SHOW HIDE FOR TABS----*/
+		jQuery(document).on('click', '.info-row', function () {
+			jQuery(this).toggleClass('active');
+		});
+	}
 });
+
+function rsmUpdateProductInfo() {
+	var base = getOptionAttribute('.BaseTab');
+	var color = getOptionAttribute('.Color');
+	var pattern = getOptionAttribute('.Pattern');
+	var shade = getOptionAttribute('.Shade');
+	var spec = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+	var html = '<div class="vpc-info-block">' +
+		'<div class="info-row"><div class="info-label">BASE</div><div class="info-data"><div class="info-name">' + base.name + '</div><div class="info-desc">' + base.description + '</div></div></div>' +
+		'<div class="info-row"><div class="info-label">COVER</div><div class="info-data"><div class="info-name">' + color.name + ' ' + pattern.name + '</div><div class="info-desc">' + pattern.description + '</div></div></div>' +
+		'<div class="info-row"><div class="info-label">SHADE</div><div class="info-data"><div class="info-name">' + shade.name + '</div><div class="info-desc">' + shade.description + '</div></div></div>' +
+		'<div class="info-row"><div class="info-label">SPECS</div><div class="info-data"><a href="' + rsmJson.siteurl + '?' + 'action=download-spec-pdf&base=' + base.name + '&color=' + color.name + '&pattern=' + pattern.name + '&shade=' + shade.name + '" target="_blank">Download Spec</a></div></div>' +
+		'</div>';
+
+	jQuery('.vpc-info-block').remove();
+	jQuery('#vpc-price-container').prepend(html);
+}
+
+function getOptionAttribute(str_selector) {
+	str_selector += ' .vpc-options input:checked';
+	var obj = jQuery(str_selector).parent().next('.rsm-vpc-info');
+	return {
+		name: obj.data('name'),
+		price: obj.data('price'),
+		description: obj.html(),
+	};
+}
+
+function rsmInitCongurator() {
+	var oldHtml = jQuery('#vpc-price-container').html();
+	jQuery('#vpc-price-container').html('');
+	jQuery('#vpc-price-container').append('<div class="vpc-action-block"><div class="vpc-price-row">' + oldHtml + '</div><div class="vpc-action-row"></div></div>');
+	jQuery('#vpc-save-btn').text('SAVE').appendTo('.vpc-action-row');
+	jQuery('#vpc-add-to-cart').text('BUY').appendTo('.vpc-action-row');
+	rsmUpdateProductInfo();
+}
--- functions.php
+++ functions.php
@@ -20,9 +20,9 @@ add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 function child_enqueue_styles() {
 	wp_enqueue_style( 'wight-design-group-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_WIGHT_DESIGN_GROUP_VERSION, 'all' );
 
-	wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery') );
-	wp_enqueue_script('rsm-js', get_stylesheet_directory_uri() . '/assets/js/rsm.js', array('jquery'), wp_rand() );
-	wp_localize_script('rsm-js', 'rsmJson', array( 'siteurl' => get_option('siteurl') ));
+	wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/assets/js/main.js', array('jquery'), wp_rand() );
+	// wp_enqueue_script('rsm-js', get_stylesheet_directory_uri() . '/assets/js/rsm.js', array('jquery'), wp_rand() );
+	wp_localize_script('main-js', 'rsmJson', array( 'siteurl' => get_option('siteurl') ));
 	//wp_enqueue_script('scroll-lock', get_stylesheet_directory_uri() . '/jquery-scrollLock.js', array('jquery') );
 	wp_enqueue_script('ui', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js', array('jquery') );
 }
